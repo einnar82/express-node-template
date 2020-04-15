6b3e7ae184dcef7bf0705dc2ebf1c3043f1d08ea
Rannie Ollit <rannie.ollit@petnet.com.ph>
2020-04-15 16:17:38 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello ExpressJS!",
+  });
+});
+
+app.listen(4000, () => {
+  console.log(`app is listening to port 4000`);
+});
